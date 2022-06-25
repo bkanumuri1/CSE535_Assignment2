2efa31f78e33b6e52525884b4eb637ee1c8c973b
Sepideh <sepidehfallah2@gmail.com>
2022-06-25 11:08:26 -0700

    post request added

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ var fs = require("fs");
 var app = express();
 var bodyParser = require("body-parser");
 
+const fileupload = require("express-fileupload");
+
+const path = require("path" );
+var http = require("http")
+var PythonShell = require("python-shell");
+const spawn = require('child process'). spawn
+const process = spawn("python" ,[" ./verify.py"])
+app.set ('view engine' ,'ejs');
+app.use (fileupload());
+
 // var exec = require("exec");
 // const { exec } = require("node:child_process");
 
@@ -13,7 +23,7 @@ app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   console.log("Get request.");
   const child = execFile(
     "python",
@@ -25,9 +35,44 @@ app.get("/", (req, res) => {
       console.log(stdout);
     }
   );
+
+  res.status(200);
+  res.render ("index")
+  // TODO: execute node script
 });
 
-const port = 3000;
 
-app.listen(port);
-console.log("Listening at port:  ", `${port}`);
+
+
+app.post ("/upload" ,async(req,res,next)=>{
+
+    try{
+
+        const file = req.files.mFile
+        const videotype = req.body.filename
+
+        const timeunique = new Date().getTime().tostring()
+
+
+        const filename = "Gautham.mp4";
+        const savepath = path.join(dirname,"public", "uploads", "GESTURE_PRACTICE_"+videotype+"_"+timeunique+"_"+filename)
+
+        console.log(savepath);
+        await file.mv(savepath)
+
+        res.send ({
+        success:true,
+        message: "File uploaded"
+        });
+
+        res.redirect('/');
+
+    } catch (e) {
+        console.log(e);
+      }
+    })
+
+    const port = 3000;
+
+    app.listen(port);
+    console.log("Listening at port:  ", `${port}`);
